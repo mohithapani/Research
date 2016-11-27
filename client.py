fdbcb78fa0050daeb2af2b6ebe21668146ce43b0
mohit hapani <mohit.hapani@gmail.com>
2016-11-26 20:00:37 -0500

    All hosts modified to work for simulation although multithreading is yet a problem

--- client.py
+++ client.py
@@ -2,9 +2,11 @@ import socket;
 import time;
 s = socket.socket();
 host = socket.gethostname();
-port = 12345;
-
-s.connect(('10.0.0.5',port));
+port1 = 12399;
+port2 = 12345;
+s1 = socket.socket()
+s.connect(('10.0.0.1',port1));
+s1.connect(('10.0.0.2',port2));
 #f = open('tosend.txt','rb');
 print "Sending";
 
@@ -12,6 +14,7 @@ while(1):
 	print "Sending...";
 	time_sen = time.ctime(time.time())
 	s.send(time_sen);
+	s1.send(time_sen);
 #f.close();
 print "Done Sending";
 s.shutdown(socket.SHUT_WR);
